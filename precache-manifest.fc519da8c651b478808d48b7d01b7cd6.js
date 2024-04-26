d644da436b6b98403e74597e20ef769ee9bd4bd4
SBIHI-Elhabib <145591766+SBIHI-Elhabib@users.noreply.github.com>
2024-04-26 01:46:55 +0100

    Updates

---
+++ precache-manifest.fc519da8c651b478808d48b7d01b7cd6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c3f8f33109fc845e5eab9248e87ac3",
+    "url": "/RobotsFriends/index.html"
+  },
+  {
+    "revision": "485efedfb99675701c89",
+    "url": "/RobotsFriends/static/css/main.033633ad.chunk.css"
+  },
+  {
+    "revision": "bfa7ebc9b338d31c23ad",
+    "url": "/RobotsFriends/static/js/2.3a3ecb4b.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/RobotsFriends/static/js/2.3a3ecb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f79887479bc79e6f417d",
+    "url": "/RobotsFriends/static/js/3.d89474ee.chunk.js"
+  },
+  {
+    "revision": "485efedfb99675701c89",
+    "url": "/RobotsFriends/static/js/main.000349ba.chunk.js"
+  },
+  {
+    "revision": "74ca1701da03670d1fe5",
+    "url": "/RobotsFriends/static/js/runtime-main.4238dc82.js"
+  },
+  {
+    "revision": "782fe79371a69cf9db3a34f6a7bc580d",
+    "url": "/RobotsFriends/static/media/background.782fe793.jpg"
+  },
+  {
+    "revision": "ec5f65b3a65a6c314613edd7fca982d9",
+    "url": "/RobotsFriends/static/media/cardBackground.ec5f65b3.jpg"
+  },
+  {
+    "revision": "ce43c6569134811c6828717671d38916",
+    "url": "/RobotsFriends/static/media/space age.ce43c656.ttf"
+  },
+  {
+    "revision": "647af959553c34198b3194b98b07354f",
+    "url": "/RobotsFriends/static/media/techno_hideo.647af959.ttf"
+  }
+]);
